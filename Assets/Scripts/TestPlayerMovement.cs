63301cd17695d1c3ad88580c92b7bede2775158e
Hunter Squires <HunterSquires01@gmail.com>
2023-01-31 15:20:22 -0800

    HS01312023T15:19 - Added platforms to level and created a test player. Imported another asset pack for more variety.

---
+++ Assets/Scripts/TestPlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerMovement : MonoBehaviour
+{
+    public KeyCode jump = KeyCode.Space;
+    public KeyCode moveRight = KeyCode.D;
+    public KeyCode moveLeft = KeyCode.A;
+
+    public float speed = 10.0f;
+    private Rigidbody2D testPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        testPlayer = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerVelocity = testPlayer.velocity;
+        if(Input.GetKeyDown(jump)){
+            playerVelocity.y = speed;
+        }
+        else if(Input.GetKey(moveRight)){
+            playerVelocity.x = speed;
+        }
+        else if(Input.GetKey(moveLeft)){
+            playerVelocity.x = -speed;
+        }
+        else{
+            playerVelocity.x = 0;
+            //playerVelocity.y = 0;
+        }
+        testPlayer.velocity = playerVelocity;
+    }
+}
