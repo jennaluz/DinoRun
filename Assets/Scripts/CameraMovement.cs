2ab79f17aca45994a727208ed1f3ca0d7d067143
Hunter Squires <HunterSquires01@gmail.com>
2023-01-31 11:07:39 -0800

    HS01312023 Added a camera background and the ground to game. Gave the Camera movement.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //public KeyCode moveRight = KeyCode.RightArrow;
+    //public KeyCode moveLeft = KeyCode.LeftArrow;
+    public float cameraSpeed = 5.0f;
+    Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cameraPosition = mainCamera.gameObject.transform.position;       
+        /*if(Input.GetKey(moveRight)){
+            cameraPosition.x += cameraSpeed;
+        }
+        else if(Input.GetKey(moveLeft)){
+            cameraPosition.x += -cameraSpeed;
+        }*/
+        cameraPosition.x += cameraSpeed;
+        mainCamera.gameObject.transform.position = cameraPosition;
+    }
+}
