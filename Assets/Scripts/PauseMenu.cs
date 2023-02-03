47b9cad4b86189ded688a5e75a44bdeb817a68e9
Alphxnse <58533701+alphxnse1301@users.noreply.github.com>
2023-02-03 13:53:11 -0800

    pause menu and button functions

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject PauseMenuCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Tab)){
+            if(Input.GetKey(KeyCode.Escape)){
+                Application.Quit();
+            } else if(isPaused){
+                ResumeGame();
+            } else {
+                Halt();
+            }
+        }
+
+        if(Input.GetKey(KeyCode.Escape)){
+            Application.Quit();
+        }  
+    }
+
+    public void Halt()
+    {
+        PauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f;
+
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+
+        isPaused = false;
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        {
+            // When 'PLAY' button is clicked it will load the game
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+                // can also be done with 'SceneManager.LoadScene(LoadScene("MainMenu"));
+
+            //Need to figure out how to reset the camera position, and score
+        }
+    }
+}
