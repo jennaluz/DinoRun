a59fb35d457e9b7241e8c93d849689917d9a30e4
jennaluz <jpura827@gmail.com>
2023-02-15 19:36:44 -0800

    JP021523 Added SFX for collecting coins.

--- Assets/Scripts/CollectCoin.cs
+++ Assets/Scripts/CollectCoin.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class CollectCoin : MonoBehaviour
 {
+    public AudioController audioController;
     public GameManager gm;
+
+    void Start()
+    {
+        audioController = GameObject.FindGameObjectWithTag("Manager").GetComponent<AudioController>();
+    }
 
     private void OnTriggerEnter2D(Collider2D triggerItem)
     {
        if(triggerItem.name == "Dinosaur")
        {
         Debug.Log("Coin has been collected");
+        audioController.Play("CollectCoin");
         gm.dinoScore();
         Destroy(gameObject);
        } 
